93494daa2f24152ebc2d600fd132461a5a6e083e
beng888 <lawrenceardosa@gmail.com>
2020-12-19 10:12:58 +0800

    commit

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -9,7 +9,7 @@ import {
 import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:5000",
+  uri: "https://protected-crag-53619.herokuapp.com/",
 });
 
 const authLink = setContext(() => {
